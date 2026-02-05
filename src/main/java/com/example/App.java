9faf16af8e745603d3907d2a4ac745a2ff7bf3ef
HanJeongmok <wjdahe1845@gmail.com>
2026-02-05 11:49:11 +0900

    그린케이스 작성

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public int plus(int i, int i1) {
-        return 0;
+        return 3;
     }
 }
--- src/test/java/Apptest.java
+++ src/test/java/Apptest.java
@@ -1,11 +1,13 @@
 
 import com.example.App;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Apptest {
 
     @Test
+    @DisplayName("1+2 = 3")
     void t1(){
         App app =new App();
         int rst=app.plus(1,2);
