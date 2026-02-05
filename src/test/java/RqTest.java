cf6b757fe916ab0bdac2b9fcfc441191404a4319
HanJeongmok <wjdahe1845@gmail.com>
2026-02-05 12:15:24 +0900

    rq테스트 그린

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -1,14 +1,23 @@
+import com.example.Rq;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RqTest {
     @Test
-    @DisplayName(value="rq.getActionName()")
+    @DisplayName(value="rq.getActionName()=삭제")
     void t1(){
-        Rq rq =new Rq("삭제=?id=1");
+        Rq rq =new Rq("삭제?id=1");
         String actionName=rq.getActionName();
         Assertions.assertThat(actionName).isEqualTo("삭제");
+    }
+
+    @Test
+    @DisplayName(value="rq.getActionName()=수정")
+    void t2(){
+        Rq rq =new Rq("수정?id=1");
+        String actionName=rq.getActionName();
+        Assertions.assertThat(actionName).isEqualTo("수정");
     }
 
 }
